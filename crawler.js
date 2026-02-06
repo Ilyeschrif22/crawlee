cca9fe76d4bb8ce0f812a6b862449aed7a0db1bf
Ilyeschrif22 <ilyes.chrif@esprit.tn>
2026-02-06 23:09:18 +0100

    addedREADME

--- crawler.js
+++ crawler.js
@@ -5,20 +5,13 @@ async function main() {
         async requestHandler({ request, page, enqueueLinks, pushData, log }) {
             log.info(`Processing ${request.url}`);
 
-            // Select all article elements with that class
             const articles = await page.$$('article.placard.placard-option-diamond.has-header.js-diamond.tmpl-diamond');
 
             for (const article of articles) {
-                // For example, extract the text content of the article
                 const text = await article.innerText();
-
-                // Or extract some specific info like title or price by querying inside the article
-                // const title = await article.$eval('h3', el => el.innerText).catch(() => null);
-
                 await pushData({ url: request.url, text /*, title */ });
             }
 
-            // Enqueue links to follow, if you want to crawl more pages
             await enqueueLinks();
 
         },
